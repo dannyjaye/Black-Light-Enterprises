fce1dc16fa0276ad8ca7d8c24804069ed9b0e161
Daniel Jeffery <danny@blacklightenterprises.co.uk>
2012-03-15 11:53:27 +0000

    Initial commit

---
+++ Sites/TEST/BLE/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BLE::Application.config.secret_token = '79063d5ccce9f6c2dac20b364574315fd6746ea4a50d84469bad802d7e56738c1511393db930a47f1bc9514699f52dfc5659fd21ce3c55d81267dbf2b205618b'
